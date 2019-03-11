b6f1dd494aa6eb61f09be092a78cc005dbc70baa
dormonde <44476361+dormonde@users.noreply.github.com>
2019-03-11 11:26:17 -0700

    js Skeleton

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
+const userScore = 0;
+const computerScore = 0;
+const userScore_span = document.getElementById("player-score");
+const compScore_span = document.getElementById("comp-score");
+const scoreBoard_div = document.querySelector(".score-board");
+const rock_div = document.getElementById("r");
+const paper_div = document.getElementById("p");
+const scissors_div = document.getElementById("s");
+
+
+
 
 
